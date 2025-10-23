86cea0a1d8195b71f77c461b6be6c73a131a175b
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-10-22 21:48:42 -0600

    fix(MDParagraph): I changed paragraph markdown, it was in html format

--- src/app/products/concrete/MDParagraph.ts
+++ src/app/products/concrete/MDParagraph.ts
@@ -2,6 +2,6 @@ import { Paragraph } from "../abstract/Paragraph";
 
 export class MDParagraph implements Paragraph {
     render(content: string): string {
-        return `<p>${content}</p>`;
+        return `${content}`;
     }
 }
